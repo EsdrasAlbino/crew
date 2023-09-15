87305ba28397b25ccf70017af6f3b2504ff231f0
Welton Felix <hi@weltonfelix.dev>
2023-09-15 08:57:04 -0300

    Implement player entity

--- entities/player_entity.py
+++ entities/player_entity.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import pygame
 
 PLAYER_WIDTH = 50
@@ -13,14 +14,55 @@ class Player(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         self.velocity = velocity
+        self.__width = PLAYER_WIDTH
+        self.__height = PLAYER_HEIGHT
+        self.__pos_x = 0
+        self.__pos_y = 0
 
     def update(self):
-        self.rect.x = self.pos_x
-        self.rect.y = self._pos_y
-        self.rect.width = PLAYER_WIDTH
-        self.rect.height = PLAYER_HEIGHT
+        self.rect.x = self.__pos_x
+        self.rect.y = self.__pos_y
+        self.rect.width = self.__width
+        self.rect.height = self.__height
 
-    def move(self, x, y):
-        self.pos_x += x * self.velocity
-        self._pos_y += y * self.velocity
+    def get_future_position(self, steps: Tuple[float, float]):
+        x_steps = steps[0]
+        y_steps = steps[1]
+
+        future_x = self.__pos_x
+        future_y = self.__pos_y
+
+        if x_steps is not None:
+            future_x = self.__pos_x + x_steps * self.velocity
+        if y_steps is not None:
+            future_y = self.__pos_y + y_steps * self.velocity
+
+        return (future_x, future_y)
+
+    @property
+    def position(self):
+        return (self.__pos_x, self.__pos_y)
+
+    @position.setter
+    def position(self, new_position: Tuple[float, float]):
+        self.__pos_x = new_position[0]
+        self.__pos_y = new_position[1]
+        self.update()
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, new_width: float):
+        self.__width = new_width
+        self.update()
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, new_height: float):
+        self.__height = new_height
         self.update()
