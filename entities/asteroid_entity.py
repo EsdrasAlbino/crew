9b74ff90cf79402d6ef660ea26ed790e9ce0e09d
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2023-09-16 12:11:13 -0300

    Create asteroid_entity file

---
+++ entities/asteroid_entity.py
@@ -0,0 +1,25 @@
+import pygame
+from pygame import *
+from pygame.sprite import _Group
+
+class  asteorid(pygame.sprite.Sprite):
+    def __init__(self, *groups: _Group, position_x, position_y):
+        super().__init__(*groups)
+        self.position_x = position_x
+        self.position_y = position_y
+
+    @property
+    def position_x(self):
+        return self.position_x
+    
+    @position_x.setter
+    def position_x(self, position_x):
+        self.position_x = position_x
+    
+    @property
+    def position_y(self):
+        return self.position_y
+    
+    @position_y.setter
+    def position_y(self, position_y):
+        self.position_y = position_y
