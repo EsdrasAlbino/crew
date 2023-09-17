102d23418ff10cb9e86204f0dc6efad305be496a
Tulio Oliveira <127243520+tuliooarauj@users.noreply.github.com>
2023-09-17 20:56:19 -0300

    create collision file

--- entities/asteroid_entity.py
+++ entities/asteroid_entity.py
@@ -2,7 +2,7 @@ import pygame
 from pygame import *
 from pygame.sprite import _Group
 
-class  asteorid(pygame.sprite.Sprite):
+class  asteroid(pygame.sprite.Sprite):
     def __init__(self, position_x, position_y):
         self.position_x = position_x
         self.position_y = position_y
--- util/collision.py
+++ util/collision.py
@@ -1,1 +1,25 @@
-from entities import 
+import pygame
+from pygame import *
+import entities
+from entities import *
+
+
+
+class Colision:
+    def __init__(self, position_x_obj1, position_y_obj1, position_x_obj2, position_y_obj2):
+        self.position_x_obj1 = position_x_obj1
+        self.position_y_obj1 = position_y_obj1
+        self.position_x_obj2 = position_x_obj2
+        self.position_y_obj2 = position_y_obj2
+
+    @property
+    def position_x_obj1(self):
+        return self.position_x_obj1
+    
+    @position_x_obj1.setter
+    def position_x_obj1(self, position_x_obj1):
+        self.position_x_obj1 = position_x_obj1
+    
+        
+
+
