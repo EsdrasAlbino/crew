92f0566efbe7f05107c62ec69b4606381478a134
MathBorgess <matheusborgesfig@gmail.com>
2023-09-18 10:51:47 -0300

    Crew controller entity

---
+++ __main__.py
@@ -0,0 +1,5 @@
+from entities.crew_entity import Crew
+
+if __name__ == '__main__':
+    game = Crew()
+    game.init()
---
+++ entities/crew_entity.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+
+
+class Crew(object):
+    def __init__(self):
+        self.screen = None
+        self.clock = None
+        pass
+
+    def init(self, screen_size=(800, 600)):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode(screen_size)
+        self.clock = pygame.time.Clock()
+
+        pygame.display.set_caption('Crew')
+
+        running = True
+        while running:
+
+            for event in pygame.event.get():
+            
+                if event.type == pygame.QUIT:
+                    running = False
+                    sys.exit()
