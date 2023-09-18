dd6dba3ac1e41df8e3af4a2e605319770f342bdc
MathBorgess <matheusborgesfig@gmail.com>
2023-09-18 11:39:46 -0300

    mvp start screen

--- entities/crew_entity.py
+++ entities/crew_entity.py
@@ -1,17 +1,22 @@
 import pygame
 import sys
+from entities.initial_screen_entity import StartScreen
 
 
 class Crew(object):
     def __init__(self):
         self.screen = None
+        self.screen_size = None
         self.clock = None
+        self.current_screen = None
 
     def init(self, screen_size=(800, 600)):
         pygame.init()
 
+        self.screen_size = screen_size
         self.screen = pygame.display.set_mode(screen_size)
         self.clock = pygame.time.Clock()
+        self.current_screen = StartScreen(screen_size)
 
         pygame.display.set_caption('Crew')
 
@@ -23,3 +28,4 @@ class Crew(object):
                 if event.type == pygame.QUIT:
                     running = False
                     sys.exit()
+                self.current_screen = self.current_screen.run(self.screen, self.screen_size, event)
