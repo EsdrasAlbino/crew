9fdbdd193cbb240873d25bfd1c925e7834ace6a3
Welton Felix <hi@weltonfelix.dev>
2023-09-29 11:31:45 -0300

    Add more enemies over time (#21)

--- entities/game_entity.py
+++ entities/game_entity.py
@@ -100,6 +100,7 @@ class Game(object):
         self.player_group.add(self.player)
 
         self.clock = pygame.time.Clock()
+        self.initial_time = pygame.time.get_ticks()
         self.fps = 60
         self.is_fullscreen = False
 
@@ -166,7 +167,9 @@ class Game(object):
             (self.track_left_coord, self.track_right_coord),
         )
         self.player_group.add(self.player)
+
         self.soundtrack.play(-1, 0, 1000)
+        self.initial_time = pygame.time.get_ticks()
 
     def __update_coords(self):
         self.player.position = (self.player.position[0], self.player_coords[1])
@@ -271,8 +274,10 @@ class Game(object):
 
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             return self
-        if self.asteroid_group.__len__() < 2:
-            seed = randint(0, 200)
+
+        elapsed_time = pygame.time.get_ticks() - self.initial_time
+        if self.asteroid_group.__len__() < 10:
+            seed = randint(0, int(200 - elapsed_time / 1000))
             if seed > 40 and seed < 45:
                 enemy = Asteroid(
                     5,
@@ -306,8 +311,8 @@ class Game(object):
             _bullet.width = (self.track_right_coord - self.track_left_coord) / 100
             _bullet.height = (self.track_right_coord - self.track_left_coord) / 50
 
-        if self.throttle_group.__len__() < 1:
-            seed = randint(0, 200)
+        if self.throttle_group.__len__() < 2:
+            seed = randint(0, 500)
             if seed == 50:
                 throttle = Throttle(
                     3,
@@ -325,8 +330,8 @@ class Game(object):
                 )
                 self.throttle_group.add(throttle)
 
-        if self.ammo_group.__len__() < 1:
-            seed = randint(0, 200)
+        if self.ammo_group.__len__() < 5:
+            seed = randint(0, int(800 - elapsed_time / 1000))
             if 25 < seed < 30:
                 ammo = Ammo(
                     3,
