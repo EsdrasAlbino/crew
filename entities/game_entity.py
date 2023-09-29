eb734ad818e0540d217d6cb69925c10a5cc5dbc0
Maria Fernanda <125303577+MariaFFA@users.noreply.github.com>
2023-09-29 12:42:13 +0000

    responsividade dos assets

--- entities/game_entity.py
+++ entities/game_entity.py
@@ -239,6 +239,18 @@ class Game(object):
         for _asteroid in self.asteroid_group.sprites():
             _asteroid.width = comet_dimensions[0]
             _asteroid.height = comet_dimensions[1]
+        for _player in self.player_group.sprites():
+            _player.width = player_dimensions[0]
+            _player.height = player_dimensions[1]
+        for _ammo in self.ammo_group.sprites():
+            _ammo.width = bullet_dimensions[0]
+            _ammo.height = bullet_dimensions[1]
+        for _throttle in self.throttle_group.sprites():
+            _throttle.width = propellant_dimensions[0]
+            _throttle.height = propellant_dimensions[1]
+        for _bullet in self.bullet_group.sprites():
+            _bullet.width = (self.track_right_coord - self.track_left_coord)/100
+            _bullet.height = (self.track_right_coord - self.track_left_coord)/50
 
         if self.throttle_group.__len__() < 1:
             seed = randint(0, 200)
