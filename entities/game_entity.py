6242ed782be2fcc8a0d68c2edb4f6af717c21a59
Maria Fernanda <125303577+MariaFFA@users.noreply.github.com>
2023-09-29 13:20:46 +0000

    responsividade das coordenadas

--- entities/game_entity.py
+++ entities/game_entity.py
@@ -231,22 +231,6 @@ class Game(object):
                 )
                 self.asteroid_group.add(enemy)
 
-        for _asteroid in self.asteroid_group.sprites():
-            _asteroid.width = comet_dimensions[0]
-            _asteroid.height = comet_dimensions[1]
-        for _player in self.player_group.sprites():
-            _player.width = player_dimensions[0]
-            _player.height = player_dimensions[1]
-        for _ammo in self.ammo_group.sprites():
-            _ammo.width = bullet_dimensions[0]
-            _ammo.height = bullet_dimensions[1]
-        for _throttle in self.throttle_group.sprites():
-            _throttle.width = propellant_dimensions[0]
-            _throttle.height = propellant_dimensions[1]
-        for _bullet in self.bullet_group.sprites():
-            _bullet.width = (self.track_right_coord - self.track_left_coord)/100
-            _bullet.height = (self.track_right_coord - self.track_left_coord)/50
-
         if self.throttle_group.__len__() < 1:
             seed = randint(0, 200)
             if seed == 50:
@@ -284,6 +268,31 @@ class Game(object):
                     self.track_bottom_coord,
                 )
                 self.ammo_group.add(ammo)
+
+        for _asteroid in self.asteroid_group.sprites():
+            _asteroid.width = comet_dimensions[0]
+            _asteroid.height = comet_dimensions[1]
+            _asteroid.posicion = (comet_new_coords[0],comet_new_coords[1])
+
+        for _player in self.player_group.sprites():
+            _player.width = player_dimensions[0]
+            _player.height = player_dimensions[1]
+            _player.posicion = (player_new_coords[0],player_new_coords[1])
+
+        for _ammo in self.ammo_group.sprites():
+            _ammo.width = bullet_dimensions[0]
+            _ammo.height = bullet_dimensions[1]
+            _ammo.posicion = (bullet_new_coords[0],bullet_new_coords[1])
+
+        for _throttle in self.throttle_group.sprites():
+            _throttle.width = propellant_dimensions[0]
+            _throttle.height = propellant_dimensions[1]
+            _throttle.posicion = (propellant_new_coords[0],propellant_new_coords[1])
+
+        for _bullet in self.bullet_group.sprites():
+            _bullet.width = (self.track_right_coord - self.track_left_coord)/100
+            _bullet.height = (self.track_right_coord - self.track_left_coord)/50
+            
 
         # update player
         self.player.update()
